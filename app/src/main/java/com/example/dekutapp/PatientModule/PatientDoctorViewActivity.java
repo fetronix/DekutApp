fea519671b8b38237b61f75a339f0681b2b360b1
Fidelis Mwendwa Nzomo <fidelism99@gmail.com>
2023-10-28 19:49:51 +0300

    hwkko

---
+++ app/src/main/java/com/example/dekutapp/PatientModule/PatientDoctorViewActivity.java
@@ -0,0 +1,16 @@
+package com.example.dekutapp.PatientModule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dekutapp.R;
+
+public class PatientDoctorViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_doctor_view);
+    }
+}
