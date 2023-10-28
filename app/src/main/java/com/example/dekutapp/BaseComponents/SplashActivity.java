72d9f437241cc78abf7f602ca1332aa5d69f282e
Fidelis Mwendwa Nzomo <fidelism99@gmail.com>
2023-10-28 19:36:00 +0300

    hwkko

--- app/src/main/java/com/example/dekutapp/BaseComponents/SplashActivity.java
+++ app/src/main/java/com/example/dekutapp/BaseComponents/SplashActivity.java
@@ -2,8 +2,17 @@ package com.example.dekutapp.BaseComponents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.dekutapp.AdminModule.AddDoctorActivity;
+import com.example.dekutapp.AdminModule.AdminDashboardActivity;
+import com.example.dekutapp.DoctorModule.DoctorDashboardActivity;
+import com.example.dekutapp.DoctorModule.DoctorLoginActivity;
+import com.example.dekutapp.PatientModule.PatientDashboardActivity;
+import com.example.dekutapp.PatientModule.PatientLoginActivity;
 import com.example.dekutapp.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -12,6 +21,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Button doctorsplash ;
+        doctorsplash = findViewById(R.id.btnroledoctor);
+
+        doctorsplash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SplashActivity.this, PatientLoginActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
